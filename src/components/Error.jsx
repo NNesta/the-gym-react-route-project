a020f327bf652172ca33bdd432a8295e6389ff5b
Ngabonziza Nestor <ngabonest@gmail.com>
2023-04-26 20:53:16 +0200

    feat: adding the error element in loader

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>{error.message}</h1>
+    </div>
+  );
+};
+
+export default Error;
