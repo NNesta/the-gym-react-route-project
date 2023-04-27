e9aeffaf5061f2a88fb3a5f825b723c3684598f1
Ngabonziza Nestor <ngabonest@gmail.com>
2023-04-27 13:28:35 +0200

    feat:adding the fetch hook to all fetchings

--- src/pages/host/HostVanInfo.jsx
+++ src/pages/host/HostVanInfo.jsx
@@ -4,9 +4,9 @@ const HostVanInfo = () => {
   const { currentVan } = useOutletContext();
   return (
     <section className="host-van-detail-info">
-      <h4>Name: {currentVan.name}</h4>
-      <h4>Category: {currentVan.type}</h4>
-      <h4>Description: {currentVan.description}</h4>
+      <h4>Name: {currentVan?.name}</h4>
+      <h4>Category: {currentVan?.type}</h4>
+      <h4>Description: {currentVan?.description}</h4>
       <h4>Visibility: Public</h4>
     </section>
   );
--- src/pages/host/Vans.jsx
+++ src/pages/host/Vans.jsx
@@ -1,16 +1,9 @@
-import { useState, useEffect } from "react";
+import useFetchHook from "../../hooks/fetchHook";
 import { Link } from "react-router-dom";
 
 export default function HostVans() {
-  const [vans, setVans] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/host/vans")
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
-  }, []);
-
-  const hostVansEls = vans.map((van) => (
+  const { data: vans } = useFetchHook("/api/host/vans");
+  const hostVansEls = vans?.map((van) => (
     <Link to={van.id} key={van.id} className="host-van-link-wrapper">
       <div className="host-van-single" key={van.id}>
         <img src={van.imageUrl} alt={van.name} />
@@ -26,7 +19,7 @@ export default function HostVans() {
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
       <div className="host-vans-list">
-        {vans.length > 0 ? (
+        {vans?.length > 0 ? (
           <section>{hostVansEls}</section>
         ) : (
           <h2>Loading...</h2>
