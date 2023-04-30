2ae557c8b7843066bbd0dd115979a3f01b3b5c05
Ngabonziza Nestor <ngabonest@gmail.com>
2023-04-30 17:15:09 +0300

    adding actions in form

--- src/api.js
+++ src/api.js
@@ -16,6 +16,7 @@ export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
   const res = await fetch(url);
   if (!res.ok) {
+    throw {
       message: "Failed to fetch vans",
       statusText: res.statusText,
       status: res.status,
